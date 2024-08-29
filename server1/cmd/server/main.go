179bacdb51120a2b98b8da1b0ccdb72dfc840ff9
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-29 23:48:42 +0530

    added config.yml

--- server1/cmd/server/main.go
+++ server1/cmd/server/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	srv := &http.Server{
+		Addr:        ":8080",
+		Handler:     handler,
+		ReadTimeout: 10 * time.SSecond,
+	}
+}
